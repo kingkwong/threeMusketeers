6adae5e918145982c389958bf765058840f2eebe
doniatung <doniatung32@gmail.com>
2017-04-20 08:49:26 -0400

    added Ticket

---
+++ Ticket.java
@@ -0,0 +1,27 @@
+public class Ticket implements Comperable<T>{
+
+    private int _iD;
+    private String _description;
+    private int _priority;
+    private String _name;
+
+
+    public Ticket(String name){
+	_name = name;
+    }
+
+    public int compareTo(Ticket other){
+
+
+    }
+
+
+    public boolean equals(Ticket other){
+
+
+    }
+
+   
+    
+
+}
