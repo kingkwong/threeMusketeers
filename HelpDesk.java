f672c92c99d9bc07800b7c1c929d6379ba588fa4
doniatung <doniatung32@gmail.com>
2017-04-22 08:37:54 -0400

    updated

--- HelpDesk.java
+++ HelpDesk.java
@@ -6,7 +6,7 @@ public class HelpDesk{
     private static ArrayList<String> _listOfServices;
 
     public HelpDesk(){
-
+	data = new ArrayPriorityQueue<Ticket>;
 	constructServiceList();
     }
 	
@@ -15,8 +15,27 @@ public class HelpDesk{
 	_listOfServices.add("dispatch tech");
 	_listOfServices.add("reinstall operating system");
 	_listOfServices.add("moral support and a hug");
-    }		    
+    }
 
+    public String passwordReset(){
+	_solved = true;
+    }
+
+    public String dispatchTech(){
+	_solved = true; //not sure if this is valid here
+	return "Okay. You can return to your desk, and I'll send someone over to see if they can fix the problem";
+    }
+
+    public String reinstallOp(){
+	_solved = true;
+	return "I reinstalled your operating system, so it should basically start over as a new machine now. Let me know if you have any other problems";
+    }
+
+    public String moralSupport(){
+	_solved = true;
+	return "Take a deep breath. Everything is working. It's all okay. *gives hug*";
+    }
+    
 
 
 }
