ab4acf99c6e03f34986e9c14fd9f86c9bef319e4
Manahal Tabassum <mtabassum@stuy.edu>
2017-04-23 18:48:03 -0400

    Added accessors+mutators to ticket

--- Ticket.java
+++ Ticket.java
@@ -8,30 +8,59 @@ public class Ticket implements Comparable<Ticket>{
     public boolean _solved;
 
 
+    /*-----------------------CONSTRUCTOR---------------------------*/
+    
     public Ticket(String name, int iD, int priority, String description){
 	_name = name;
 	_iD = iD;
 	_priority = priority;
 	_description = description;
+	_solved = false;
+	_solution = "unknown";
     }
-
-    private int getPriority(){
+    
+    /*--------------------------ACCESORS---------------------------*/
+    
+    public int getPriority(){
 	return _priority;
     }
 
-    private int getID(){
+    public int getID(){
 	return _iD;
     }
 
+    public String getName(){
+	return _name;
+    }
+
+    public String getDescription(){
+	return _description;
+    }
+
+    /*-------------------------MUTATORS------------------------------*/
+
+    public void setPriority(int newPriority){
+	_priority = newPriority;
+    }
+
+    public void setSolution(String solution){
+	_solution = solution;
+    }
+
+    public void setSolved(boolean solved){
+	_solved = solved;
+    }
+
+    /*----------------------------------------------------------------*/
+    
+    //compares tickets on the basis of priority
     public int compareTo(Ticket other){
 	return _priority - other.getPriority();
     }
 
-
+    //returns whether two tickets are the same on the basis of the ID number
     public boolean equals(Ticket other){
 	return _iD == other.getID();
     }
-
-    
 
 }
